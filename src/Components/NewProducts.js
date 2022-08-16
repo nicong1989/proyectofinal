82d7a4b592f5cfd35883a38f3c02c33384ebe922
nicong1989 <ngomeznicolas89@gmail.com>
2022-08-16 18:06:00 -0300

    listadeproductos

---
+++ src/Components/NewProducts.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import Listadeproductos from './Listadeproductos';
+
+const Newproducts = () => {
+  return (
+    <div>
+        
+        <Container fluid>
+        <h1>Nuevos Product bnbnos</h1>
+        <Listadeproductos />
+        </Container>
+
+    </div>
+  )
+}
+
+export default Newproducts
