41ab4e8baddeb3e7d59251a459e43ae4075025af
nicong1989 <ngomeznicolas89@gmail.com>
2022-08-17 20:19:52 -0300

    pruebadeestilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
-import ListProductos from './components/ListProductos';
+import React from "react";
+// import { Container } from 'react-bootstrap';
+// import ListProductos from './components/ListProductos';
+import { makeStyles } from "@mui/styles";
+import Container from "@mui/material/Container";
+
+const useStyles = makeStyles({
+  root: {
+    background:
+      "linear-gradient(to right, rgb(15, 32, 39), rgb(32, 58, 67), rgb(44, 83, 100))",
+    textAlign: "center",
+    
+    height: "100vh",
+    width: "100%",
+    maxWidth: "1400px !important",
+    color: '#a7ff83',
+  },
+});
 
 const App = () => {
-    return (
-        <Container fluid>
-            <h2 className="text-center">Prueba de productos con JSON SERVER Y AXIOS</h2>
-            <ListProductos />
-        </Container>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Container className={classes.root}>
+      <h2>Prueba de Estilos</h2>
+    </Container>
+  );
+};
 
 export default App;
