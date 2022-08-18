4e82b39badf43709a719aaaf0c31921f436afae7
nicong1989 <ngomeznicolas89@gmail.com>
2022-08-18 18:32:53 -0300

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,57 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
 import Container from "@mui/material/Container";
-import Cards from "./components/componentesdeprueba/Cards";
-import Products from "./components/componentesdeprueba/Products"
-import contents from './content';
-import Sucripcion from "./components/componentesdeprueba/Sucripcion";
-
-
-
+import Cards from "./components/componentesdeprueba/Cards"
+import Products from "./components/componentesdeprueba/Products";
 
 const useStyles = makeStyles({
-  root: {
+  style: {
     background:
       "linear-gradient(to right, rgb(15, 32, 39), rgb(32, 58, 67), rgb(44, 83, 100))",
     textAlign: "center",
     height: "100vh",
     width: "100%",
     maxWidth: "1400px !important",
-    color: 'black',
+    
   },
 
+  titulo: {
+    color: '#000000',
+    display: "flex !important",
+    justifyContent:'center !important',
+    flexDirection:'column',
+    alignItems:'center'
+        
+  }
 
 });
-
 
 const App = () => {
   const classes = useStyles();
   return (
+    <>
     
     
-    <Container className={classes.root}>
-     <h2>Prueba de Estilos</h2>
-    <h3 className>Listado de Productos con JSON SERVER Y AXIOS</h3>
-   
-    <Cards />
-    <div className='App'>
-                {contents.map(contents => (
-                    <Products 
-                        key={contents.id}
-                        image={contents.image}
-                        name={contents.name}
-                        price={contents.price}
-                        totalSales={contents.totalSales}
-                        timeLeft={contents.timeLeft}
-                        rating={contents.rating}
-                    />
-                ))}
-            </div>
-                  <Sucripcion />
+    <Container className={classes.titulo}>
+    <h2 >Prueba de Estilos</h2>
+    <h3>Lista de Productos con JSON SERVER y AXIOS</h3>
     </Container>
-    
-   
 
 
-    
 
-  )
+    <Container className={classes.style}>
+    <Cards />
+    </Container>
+
+    <Container className={classes.style}>
+    <Products />
+    </Container>
+
+
+
+
+    </>
+  );
 };
 
 export default App;
