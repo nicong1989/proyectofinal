8cfc6faedfe2fd93c3079dc48365769e95cd0020
nicong1989 <you@example.com>
2022-08-23 15:50:03 -0300

    sigo

--- src/components/Cards/Producto.jsx
+++ src/components/Cards/Producto.jsx
@@ -58,32 +58,29 @@ const CardProductos = ({ productos }) => {
   const [isRed, setIsRed] = useState(true);
 
   return (
-<Card sx={{ maxWidth: 345 }}>
+<Card sx={{ maxWidth: 345, maxHeight: 600 }}>
       <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
+        
         action={
           <IconButton aria-label="settings">
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={productos.name}
+        subheader={productos.model}
       />
       <CardMedia
         component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
+        height="150"
+        image={productos.image}
+        
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          {productos.totalSales} units sold
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        {productos.timeLeft} days left
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
