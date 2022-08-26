d6b9ce1a82049869719dd5e402054a6045f3e462
nicong1989 <you@example.com>
2022-08-26 13:09:36 -0300

    prueba

--- src/components/modal/Container.jsx
+++ src/components/modal/Container.jsx
@@ -1,98 +1,32 @@
 import React from "react";
-import { useState } from "react";
-import Typography from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import IconButton from "@mui/material/IconButton";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import PruebaModal from "../modal/PruebaModal";
-import { FaShoppingCart, FaStar } from "react-icons/fa";
+import { Container } from "@mui/material";
 
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-
-const Container = ({ productos }) => {
+const Containers = ({ productos }) => {
 
 
 
-  const [isRed, setIsRed] = useState(true);
+  
 
   return (
-    <Card sx={{
-      maxWidth: 345,color: "#ffff", maxHeight: 600, backgroundColor: '#ffffff00', transition: '0.4s', '&:hover': {
-        transform: 'translateY(-0.5rem)',
-        boxShadow: '0.5rem 0.75rem 1.5rem #17b978',
-      },
-    }}>
-      <CardHeader
-        action={
-          <IconButton aria-label="settings" sx={{ color: "white", textShadow: "4px 4px 6px #000000" }}>
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={<strong>{productos.name}</strong>}
-        subheader={<Typography color="#ffff" >{productos.model}</Typography>}
-        sx={{ color: "white" }}
-      />
-      <CardMedia component="img" height="150" sx={{ maxWidth: "79%", filter: "saturate(250%)" }} image={productos.image} />
-      <CardContent sx={{ display: "flex", justifyContent: "space-between", textShadow: "4px 4px 6px #000000" }}>
-        <Typography variant="body2" color="#ffff" sx={{ textShadow: "4px 4px 6px #000000" }} >
-          {<strong>{productos.totalSales}</strong>} units sold
-        </Typography>
-        <Typography variant="body2" color="#ffff">
-          {<strong>{productos.timeLeft}</strong>} days left
-        </Typography>
-      </CardContent>
-      <CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Typography variant="body2" color="#ffff" sx={{ fontSize: "1.5rem" }}>
-          {<strong>{productos.price}</strong>}
-        </Typography>
-        <Typography variant="body2" color="rgba(247, 98, 5, 1)">
-          {[...Array(productos.rating)].map((index) => (
-            <FaStar id={index + 1} key={index} />
-          ))}
-        </Typography>
-      </CardContent>
+    <Container>
+      <p>{productos.name}</p>
+      <p>{productos.model}</p>
+      <p>{productos.price}</p>
+      <p>{productos.unitsSold}</p>
+      <p>{productos.rating}</p>
 
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites" style={{
-          textShadow: "4px 4px 6px #000000", color: isRed ? "#ffff" : "black",
-        }} onClick={() => {
-          setIsRed(!isRed);
-        }}>
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="buy" sx={{
-          color: "#ffff",transition:'0.5s' ,textShadow: "4px 4px 6px #000000", '&:hover': {
-            color: 'black',
-          }
-        }}>
-          <FaShoppingCart/>
-        </IconButton >
-        <ExpandMore sx={{display:'flex', flexDirection:'column'}}>
-          <PruebaModal />
-        </ExpandMore>
-      </CardActions>
-    </Card>
+    
+
+
+
+
+
+    </Container>
   );
 };
 
-export default Container;
+export default Containers;
 
 
 
