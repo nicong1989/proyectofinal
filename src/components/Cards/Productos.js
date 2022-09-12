4c6787a694bfeaebe526247eafc25adc0052d3a6
nicong1989 <ngomeznicolas89@gmail.com>
2022-09-12 18:53:19 -0300

    pruebas

--- src/components/Cards/Productos.js
+++ src/components/Cards/Productos.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Producto from "./Producto";
 import { Autoplay, Pagination } from "swiper";
